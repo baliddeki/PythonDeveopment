a7ab8c13d8faae825ee72f41d19e632f97ce3050
Bryan <baliddeki@gmail.com>
2023-06-23 10:47:50 +0300

    I have made more chages...this time covering even logical operators, to add to the comparison operators before

--- aliddeki_bryan_day3_morning.py
+++ aliddeki_bryan_day3_morning.py
@@ -1,78 +1,10 @@
-# // return the quotient without the remainder
+#Examples with Logical Operators
+a = True
+b = True
 
-"""
-Comparison Operators
-== equal to
-!= not equal to
-> greater than
->= greater than or equal to
-<= less than or equal to
-
-Logical Operators
-Logical AND 'and' 
-Logical OR 'or'
-Logical NOT 'not'
-
-Assignment Operators
-Assign value: '='
-Add value: '+'
-Add and assign: '+='
-Subtract and assign: '-='
-Divide and assign: '/='
-Modulus and assign: '%='
-Exponentiate and assign: '**='
-
-Membership Operators
-In: 'in' - checks if a value exists in a sequence
-Not in: 'not in' - checks if a value does not exist in a sequence
+#Logical AND
+print(a and b)
 
 
-Identity Operators
-Is: 'is' operator: checks if two values are not the same
-
-
-
-"""
-
-
-print(2**5)
-print(3+5)
-print(10%3)
-print(10/3)
-print(10//3)
-print(10*3)
-print(10-3)
-print(10+3)
-a = 5
-print(a)
-
-#Comparison Operators
-a = 23.5
-b = 15
-c = 9
-
-#Greater than
-if (a > b):
-    print("a is greater than b")
-
-#Less than
-if (c < a):
-    print("c is less than a")
-
-#Greater than or equal to
-if (a >= b):
-    print("a is greater than or equal to b")
-
-#Less than or equal to
-if (c <= a):
-    print("c is less than or equal to a")
-
-#Equal to
-if (a == b):
-    print("a is equal to b")
-
-#Not equal to
-if (a != b):
-    print("a is not equal to b")
 
 
