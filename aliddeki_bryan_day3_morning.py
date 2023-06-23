831e53bbb9b213b81ce0054c5c9f932a8964660a
Bryan <baliddeki@gmail.com>
2023-06-23 10:02:27 +0300

    I have covered a bit of operators in python

---
+++ aliddeki_bryan_day3_morning.py
@@ -0,0 +1,47 @@
+# // return the quotient without the remainder
+
+"""
+Comparison Operators
+== equal to
+!= not equal to
+> greater than
+>= greater than or equal to
+<= less than or equal to
+
+Logical Operators
+Logical AND 'and' 
+Logical OR 'or'
+Logical NOT 'not'
+
+Assignment Operators
+Assign value: '='
+Add value: '+'
+Add and assign: '+='
+Subtract and assign: '-='
+Divide and assign: '/='
+Modulus and assign: '%='
+Exponentiate and assign: '**='
+
+Membership Operators
+In: 'in' - checks if a value exists in a sequence
+Not in: 'not in' - checks if a value does not exist in a sequence
+
+
+Identity Operators
+Is: 'is' operator: checks if two values are not the same
+
+
+
+"""
+
+
+print(2**5)
+print(3+5)
+print(10%3)
+print(10/3)
+print(10//3)
+print(10*3)
+print(10-3)
+print(10+3)
+a = 5
+print(a)
