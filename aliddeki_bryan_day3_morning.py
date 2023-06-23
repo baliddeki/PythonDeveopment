e8806dd68172c72200dad6796b7c8a78438993b7
Bryan <baliddeki@gmail.com>
2023-06-23 13:24:50 +0300

    I have updated the code to include calculation functionalities of addition, subtraction, addition and mutliplication. Now off to do some error validation

--- aliddeki_bryan_day3_morning.py
+++ aliddeki_bryan_day3_morning.py
@@ -1,4 +1,5 @@
-import tkinter as tink
+import tkinter as tk
+from tkinter import messagebox
 """# // return the quotient without the remainder
 
 
@@ -145,60 +146,72 @@ print(a >> 2)
 #Make your title of the calculator program with window with your name eg 'AliBryan' simple calculator
 
 
-#creating window GUI for the calculator
-cal_window = tink.Tk()
-operation = tink.StringVar()
+
+#adding logic
+def add():
+    a = float(num_input1.get())
+    b = float(num_input2.get())
+    result = a + b
+    messagebox.showinfo("Result", str(result))
+
+#subtracting logic
+def subtract():
+    a = float(num_input1.get())
+    b = float(num_input2.get())
+    result = a - b
+    messagebox.showinfo("Result", str(result))
+
+#multiplying logic
+def multiply():
+    a = float(num_input1.get())
+    b = float(num_input2.get())
+    result = a * b
+    messagebox.showinfo("Result", str(result))
+
+#dividing logic
+def divide():
+    a = float(num_input1.get())
+    b = float(num_input2.get())
+    result = a / b
+    messagebox.showinfo("Result", str(result))
+
+#Creating Calculator GUI
+#Creating a window
+cal_window = tk.Tk()
+operation = tk.StringVar()
 cal_window.title("AliddekiBryan Simple Calculator")
 
-#setting the size of the window
+#adjusting the window size
 cal_window.geometry("400x400")
 
-#setting the text input fields
-num_input1_label = tink.Label(cal_window, text="Enter a number:")
-num_input1 = tink.Entry(cal_window)
-num_input2_label = tink.Label(cal_window, text="Enter another number:")
-num_input2 = tink.Entry(cal_window)
+#setting the text input entry
+num_input1_label = tk.Label(cal_window, text="Enter a number:")
+num_input1 = tk.Entry(cal_window)
+num_input2_label = tk.Label(cal_window, text="Enter another number:")
+num_input2 = tk.Entry(cal_window)
 
-#setting the radio buttons
-title_label = tink.Label(cal_window, text="Select an operation below:")
-add_button = tink.Radiobutton(cal_window, text="+", variable=operation, value="add_button")
-subtract_button = tink.Radiobutton(cal_window, text="-", variable=operation, value="subtract_button")
-multiply_button = tink.Radiobutton(cal_window, text="*", variable=operation, value="multiply_button")
-divide_button = tink.Radiobutton(cal_window, text="/", variable=operation, value="divide_button")
+#setting the buttons
+title_label = tk.Label(cal_window, text="Select an operation below:")
+add_button = tk.Radiobutton(cal_window, text="+", variable=operation, value="add_button", command=add)
+subtract_button = tk.Radiobutton(cal_window, text="-", variable=operation, value="subtract_button", command=subtract)
+multiply_button = tk.Radiobutton(cal_window, text="*", variable=operation, value="multiply_button", command=multiply)
+divide_button = tk.Radiobutton(cal_window, text="/", variable=operation, value="divide_button", command=divide)
 
-#positioning the text input fields
-num_input1_label.grid(row=0, column=0)
-num_input1.grid(row=0, column=1)
-num_input2_label.grid(row=1, column=0)
-num_input2.grid(row=1, column=1)
+#placing the widgets
+num_input1_label.grid(row=0, column=0, padx=10, pady=10)
+num_input1.grid(row=0, column=1, padx=10, pady=10)
+num_input2_label.grid(row=1, column=0, padx=10, pady=10)
+num_input2.grid(row=1, column=1, padx=10, pady=10)
 
-
-#positioning the radio buttons 
-title_label.grid(row=2, column=0)
-add_button.grid(row=3, column=0)
-subtract_button.grid(row=3, column=1)
-multiply_button.grid(row=3, column=2)
-divide_button.grid(row=3, column=3)
+#placing the buttons
+title_label.grid(row=2, column=0, columnspan=4, padx=10, pady=10)
+add_button.grid(row=3, column=0, padx=10, pady=10)
+subtract_button.grid(row=3, column=1, padx=10, pady=10)
+multiply_button.grid(row=3, column=2, padx=10, pady=10)
+divide_button.grid(row=3, column=3, padx=10, pady=10)
 
 cal_window.mainloop()
-"""def add():
-    return a + b
-def subtract():
-    return a - b
-def multiply():
-    return a * b
-def divide():
-    return a / b"""
 
 
 
-
-
-
-
-"""label = tink.Label(window, text="Hello, Tkinter!")
-button = tink.Button(window, text="Click Me!")
-label.pack()
-button.pack()
-window.mainloop()"""
 
