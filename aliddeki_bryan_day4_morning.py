e9fcb6ea27630d3ae6c775c48b50842919945d5e
Bryan <baliddeki@gmail.com>
2023-06-26 12:06:59 +0300

    I have added a program to convert from degrees celcius to fahrenheight while considering encapsulation

--- aliddeki_bryan_day4_morning.py
+++ aliddeki_bryan_day4_morning.py
@@ -1,12 +1,13 @@
 """Day4 Monday 26th June 2023
 
-# Object Oriented Programming (OOP)
+Object Oriented Programming (OOP)
 
-# Classes - blueprint for creating objects
+Classes - blueprint for creating objects
+"""
 
 
 class Car:
-    #A class that represents a car.
+    # A class that represents a car.
 
     def __init__(self, car_make, car_model, car_year):
         self.car_make = car_make
@@ -14,11 +15,11 @@ class Car:
         self.car_year = car_year
 
     def start_engine(self):
-        #Starts the car's engine.
+        # Starts the car's engine.
         print("Engine has started")
 
     def stop_engine(self):
-        #Stops the car's engine.
+        # Stops the car's engine.
         print("Engine has stopped")
 
 
@@ -30,10 +31,12 @@ print(my_car.car_year)
 print(my_car.start_engine())
 print(my_car.stop_engine())
 
+print("------------------------------------------------------------------")
+
 
 # Simple Program to show student details
 class Student:
-    classs to show student details
+    """classs to show student details"""
 
     def __init__(self, name, regno):
         self.name = name
@@ -46,6 +49,7 @@ class Student:
 
 student_obj = Student("Bryan", "21/U/0663")
 student_obj.display()
+print("----------------------------------------------------------")
 
 
 # Exercise
@@ -67,28 +71,33 @@ class AreaCircum:
 
 
 obj = AreaCircum(21, 3.14)
-obj.area
-obj.circum
+obj.area()
+obj.circum()
+print("--------------------------------------------------")
 
+
+# Program to print area and perimeter of a rectangle
 class AreaPerim:
     def __init__(self, length, width):
-        self.lengths = length
+        self.length = length
         self.width = width
 
     def area(self):
-        area = self.length*self.width
+        area = self.length * self.width
         print("Area of the circle is", area)
-    
-    def perim(self):
-        perim = 2*(self.length + self.width)
-        print("Perimter of rectangle is", perim)
-        
-rect = AreaPerim
-"""
 
-"""bonus = 0.5 of salary 
-employee1 salary =  150k
-employee2 salary = """
+    def perim(self):
+        perim = 2 * (self.length + self.width)
+        print("Perimter of rectangle is", perim)
+
+
+rect = AreaPerim(20, 40)
+rect.area()
+rect.perim()
+print("------------------------------------------------------------------")
+
+
+# Program to calculate employee bonus
 
 
 class Employee:
@@ -107,3 +116,72 @@ employee1 = Employee(15000)
 employee2 = Employee(230000)
 employee1.salary_calc()
 employee2.salary_calc()
+print("-------------------------------------------------------------")
+
+# Encapsulation
+# Key Goals of encapsulation
+
+# 1. hide implementation details of an object
+# 2. protect object from changes
+# 3. protect object from external changes
+# 4. code organization and modularity
+
+# Example with a bank account
+
+
+class BankAccount:
+    """bank account constructor"""
+
+    def __init__(self, account_number, balance):
+        self._account_number = account_number
+        self._balance = balance
+
+    def deposit(self, amount):
+        """deposit method"""
+        self._balance += amount
+
+    def withdraw(self, amount):
+        """withdraw method"""
+        if self._balance >= amount:
+            self._balance -= amount
+        else:
+            print("Insufficient funds.")
+
+    def get_balance(self):
+        """get balance method"""
+        return self._balance
+
+
+my_account = BankAccount("3200100783", 200000)
+
+print("Account number:", my_account._account_number)
+print("Balance:", my_account._balance)
+my_account.deposit(50000)
+print("Balance:", my_account._balance)
+my_account.withdraw(100000)
+print("Balance:", my_account._balance)
+print("---------------------------------------------")
+
+# Exercise2: Convert temperature from degress Celcius to Fahrenheight
+
+
+class Temperature:
+    """program to convert from degree Celcus to Fahrenheight"""
+
+    def __init__(self):
+        self._temp = int(input("Enter the temperature to be converted: "))
+
+    def convert(self):
+        """method to convert celcius to fahrenheight"""
+        fahren = (self._temp * (9 / 5)) + 32
+        return fahren
+
+    def get_fahren_temp(self):
+        """method to return to new converted to temperature"""
+        return self.convert()
+
+
+tempObj = Temperature()
+print(tempObj.convert())
+
+print("-------------------------------------------------------------------------------")
