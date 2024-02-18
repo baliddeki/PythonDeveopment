499972806fad088d2cd087c0eb598920305d485e
baliddeki <baliddeki@gmail.com>
2024-02-18 06:11:04 +0300

    testing ""

---
+++ PythonDSA/structures.py
@@ -0,0 +1,9 @@
+greeting = 'hello'
+greet_list = list(greeting)
+print(greet_list)
+
+
+print("""Welcome to the new CGPA Calculator
+Plaease enter your grades one by one
+to enhance readability
+""")
